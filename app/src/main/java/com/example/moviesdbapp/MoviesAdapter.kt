fb41619ce8c3d5fb0bbf962d3ee13f8384e4e14b
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-18 23:58:05 +0200

    Added Movies Adapter Class

--- app/src/main/java/com/example/moviesdbapp/MoviesAdapter.kt
+++ app/src/main/java/com/example/moviesdbapp/MoviesAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.moviesdbapp
 
-class MoviesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.card_movies.view.*
+
+class MoviesAdapter(var moviesList: ArrayList<Movie>, var movieListener: MovieListener ):
+    RecyclerView.Adapter<MoviesAdapter.MovieViewHolder{
+
+    interface MovieListener {
+        fun movieClicked(title: String)
+    }
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        init {
+            itemView.setOnClickListener(View.OnClickListener {
+                var movie = itemView.getTag() as Movie
+                movieListener.movieClicked(movie.title)
+            })
+        }
+
+        fun onBind(movie: Movie){
+            itemView.setTag(movie)
+            itemView.movieTitle.setText(movie.title)
+            itemView.movieReleaseDate.setText(movie.releaseDate)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.card_movies, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.onBind(moviesList.get(position))
+    }
+
+    override fun getItemCount(): Int = moviesList.size
+
 }
