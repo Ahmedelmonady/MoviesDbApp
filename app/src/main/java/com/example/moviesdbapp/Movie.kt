f95b499d801d77c7af0efac87e4212be3550ac00
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-19 17:15:38 +0200

    Updated the Movie Model Fields to represent the needed fields from the Json Object

--- app/src/main/java/com/example/moviesdbapp/Movie.kt
+++ app/src/main/java/com/example/moviesdbapp/Movie.kt
@@ -1,7 +1,13 @@
 package com.example.moviesdbapp
 
+import com.google.gson.annotations.SerializedName
+
 data class Movie(
-    var title: String,
-    var poster: String,
-    var releaseDate: String
+    @SerializedName("id") val id: Long,
+    @SerializedName("title") val title: String,
+    @SerializedName("overview") val overview: String,
+    @SerializedName("poster_path") val posterPath: String,
+    @SerializedName("backdrop_path") val backdropPath: String,
+    @SerializedName("vote_average") val rating: Float,
+    @SerializedName("release_date") val releaseDate: String
 )
