e276e979104ce0337a6d3eecac27e3e40084be38
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-20 00:31:54 +0200

    Implemented onPopularMoviesFetched

--- app/src/main/java/com/example/moviesdbapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesdbapp/MainActivity.kt
@@ -10,42 +10,37 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity(), MoviesAdapter.MovieListener {
+class MainActivity : AppCompatActivity() {
 
     lateinit var moviesList: MutableList<Movie>
+    var currentPageNumber: Int = 1
+    lateinit var moviesAdapter: MoviesAdapter
+    lateinit var linearLayoutManager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getPopularMovies()
 
-        //recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        //recyclerView.adapter = MoviesAdapter(moviesList, this)
+        moviesAdapter = MoviesAdapter(mutableListOf())
+        linearLayoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
 
-    }
-
-    override fun movieClicked(movieId: Long) {
-        TODO("Not yet implemented")
+        recyclerView.adapter = moviesAdapter
+        recyclerView.layoutManager = linearLayoutManager
     }
 
     fun getPopularMovies(){
-        MoviesRepository.apiServices.getPopularMovies()
-            .enqueue(object: Callback<MoviesResponse>{
-                override fun onResponse(
-                    call: Call<MoviesResponse>,
-                    response: Response<MoviesResponse>
-                ) {
-                    if(response.isSuccessful){
-                        Log.d("repo", "${response.body()?.movies}")
-                    }
-                    else{
-                        Log.d("repo", "Response is not successful")
-                    }
-                }
+        MoviesRepository.fetchPopularMovies(
+            page = 1,
+            ::onPopularMoviesFetched,
+            ::onError
+        )
+    }
 
-                override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
-                    Toast.makeText(applicationContext, "Failed to load movies", Toast.LENGTH_LONG).show()
-                }
+    fun onError(){
+        Toast.makeText(this, "Failed to fetch Movies", Toast.LENGTH_SHORT).show()
+    }
 
-            })
+    fun onPopularMoviesFetched(moviesList : MutableList<Movie>) {
+        moviesAdapter.appendMovies(moviesList)
     }
 }
