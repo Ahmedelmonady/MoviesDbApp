1a002cf9dcc0c3743aa4372236dca1e45fa0ce02
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-19 21:35:14 +0200

    Implemented the callbacks to retreive data and handle failure errors

--- app/src/main/java/com/example/moviesdbapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesdbapp/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.moviesdbapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), MoviesAdapter.MovieListener {
 
@@ -12,13 +16,31 @@ class MainActivity : AppCompatActivity(), MoviesAdapter.MovieListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getPopularMovies()
 
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerView.adapter = MoviesAdapter(moviesList, this)
+        //recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        //recyclerView.adapter = MoviesAdapter(moviesList, this)
 
     }
 
     override fun movieClicked(movieId: Long) {
         TODO("Not yet implemented")
+    }
+
+    fun getPopularMovies(){
+        MoviesRepository.apiServices.getPopularMovies()
+            .enqueue(object: Callback<MoviesResponse>{
+                override fun onResponse(
+                    call: Call<MoviesResponse>,
+                    response: Response<MoviesResponse>
+                ) {
+                    Log.d("repo", "${response.body()?.movies}")
+                }
+
+                override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
+                    Toast.makeText(applicationContext, "Failed to load movies", Toast.LENGTH_LONG).show()
+                }
+
+            })
     }
 }
