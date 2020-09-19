3b7af6213b3c660338c0a64d156324aec308e855
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-20 01:23:04 +0200

    MovieDetailsActivity initial commit

---
+++ app/src/main/java/com/example/moviesdbapp/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.moviesdbapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+
+
+    }
+}
