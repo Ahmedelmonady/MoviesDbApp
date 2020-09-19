90fa82b0fe902b166be0e4c306e8ea204825a04b
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2020-09-20 00:41:19 +0200

    Added attachOnScrollListener function to update the moviesList onScroll

--- app/src/main/java/com/example/moviesdbapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesdbapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -26,11 +28,13 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.adapter = moviesAdapter
         recyclerView.layoutManager = linearLayoutManager
+
+        attachOnScrollListener()
     }
 
     fun getPopularMovies(){
         MoviesRepository.fetchPopularMovies(
-            page = 1,
+            currentPageNumber,
             ::onPopularMoviesFetched,
             ::onError
         )
@@ -42,5 +46,21 @@ class MainActivity : AppCompatActivity() {
 
     fun onPopularMoviesFetched(moviesList : MutableList<Movie>) {
         moviesAdapter.appendMovies(moviesList)
+    }
+
+    fun attachOnScrollListener(){
+        recyclerView.addOnScrollListener(object: OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val totalItems = linearLayoutManager.itemCount
+                val visibleItemsCount = linearLayoutManager.childCount
+                val firstVisibleItem = linearLayoutManager.findFirstVisibleItemPosition()
+
+                if(firstVisibleItem + visibleItemsCount >= totalItems / 2){
+                    recyclerView.removeOnScrollListener(this)
+                    currentPageNumber++
+                    getPopularMovies()
+                }
+            }
+        })
     }
 }
